88df64943a6499b158e61b91f0e44923f758bbda
Krivonosov Denis <Krivonosovdenis@gmail.com>
2019-07-01 12:44:48 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,86 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.lang.IllegalStateException
+import java.text.SimpleDateFormat
+import java.util.*
+//
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+
+fun Date.format(pattern:String="HH:mm:ss dd.MM.yy"):String{
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+fun Date.add(value:Int, units:TimeUnits=TimeUnits.SECOND):Date{
+    var time = this.time
+
+    time += when(units) {
+        TimeUnits.SECOND -> value* SECOND
+        TimeUnits.MINUTE -> value* MINUTE
+        TimeUnits.HOUR -> value* HOUR
+        TimeUnits.DAY -> value* DAY
+    }
+    this.time = time
+    return this
+}
+
+fun Date.humanizeDiff(date:Date = Date()): String {
+
+    var time = this.time
+    val milliSecondsFiff = date.getTime() - time
+    var secondsDiff = milliSecondsFiff/1000
+    if(secondsDiff<0){secondsDiff-=1}
+//    val minutesDiff = milliSecondsFiff.toInt()/1000*60
+    println(secondsDiff)
+    val range1 = arrayOf(0L,5L,6L,7L,8L,9L)
+    val range2 = arrayOf(1L)
+    val range3 = arrayOf(2L, 3L, 4L)
+    val humanized = when{
+        secondsDiff in 0..1-> "только что"
+        secondsDiff in 1..45-> "несколько секунд назад"
+        secondsDiff in 45..75-> "минуту назад"
+        secondsDiff in 75..45*60 && secondsDiff>=600 && secondsDiff<=1200 -> "${secondsDiff/60} минут назад"
+        secondsDiff in 75..45*60 && range1.contains(secondsDiff/60%10) -> "${secondsDiff/60} минут назад"
+        secondsDiff in 75..45*60 && range2.contains(secondsDiff/60%10) -> "${secondsDiff/60} минуту назад"
+        secondsDiff in 75..45*60 && range3.contains(secondsDiff/60%10) -> "${secondsDiff/60} минуты назад"
+        secondsDiff in 45*60..75*60 -> "час назад"
+        secondsDiff in 75*60..22*60*60 && secondsDiff>=10*66*66 && secondsDiff<=20*60*60 -> "${secondsDiff/(60*60)} часов назад"
+        secondsDiff in 75*60..22*60*60 && range1.contains(secondsDiff/(60*60)%10) -> "${secondsDiff/(60*60)} часов назад"
+        secondsDiff in 75*60..22*60*60 && range2.contains(secondsDiff/(60*60)%10) -> "${secondsDiff/(60*60)} час назад"
+        secondsDiff in 75*60..22*60*60 && range3.contains(secondsDiff/(60*60)%10) -> "${secondsDiff/(60*60)} часа назад"
+        secondsDiff in 22*60*60..26*60*60 -> "день назад"
+        secondsDiff in 26*60*60..360*24*60*60 && secondsDiff>=10*24*60*60 && secondsDiff<=20*24*60*60 -> "${secondsDiff/(24*60*60)} дней назад"
+        secondsDiff in 26*60*60..360*24*60*60 && range1.contains(secondsDiff/(24*60*60)%10) -> "${secondsDiff/(24*60*60)} дней назад"
+        secondsDiff in 26*60*60..360*24*60*60 && range2.contains(secondsDiff/(24*60*60)%10) -> "${secondsDiff/(24*60*60)} день назад"
+        secondsDiff in 26*60*60..360*24*60*60 && range3.contains(secondsDiff/(24*60*60)%10) -> "${secondsDiff/(24*60*60)} дня назад"
+        secondsDiff>360*24*60*60 -> "более года назад"
+
+        secondsDiff<0 && Math.abs(secondsDiff) in 0..1-> "только что"
+        secondsDiff<0 && Math.abs(secondsDiff) in 1..45-> "чере несколько секунд "
+        secondsDiff<0 && Math.abs(secondsDiff) in 45..75-> "через минуту"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75..45*60 && secondsDiff<=-600 && secondsDiff>=-1200 -> "через ${Math.abs(secondsDiff)/60} минут"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75..45*60 && range1.contains(Math.abs(secondsDiff)/60%10) -> "через ${Math.abs(secondsDiff)/60} минут"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75..45*60 && range2.contains(Math.abs(secondsDiff)/60%10) -> "через ${Math.abs(secondsDiff)/60} минуту"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75..45*60 && range3.contains(Math.abs(secondsDiff)/60%10) -> "через ${Math.abs(secondsDiff)/60} минуты"
+        secondsDiff<0 && Math.abs(secondsDiff) in 45*60..75*60 -> "час назад"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75*60..22*60*60 && Math.abs(secondsDiff)<=-10*66*66 && Math.abs(secondsDiff)>=-20*60*60 -> "через ${Math.abs(secondsDiff)/(60*60)} часов"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75*60..22*60*60 && range1.contains(Math.abs(secondsDiff)/(60*60)%10) -> "через ${Math.abs(secondsDiff)/(60*60)} часов"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75*60..22*60*60 && range2.contains(Math.abs(secondsDiff)/(60*60)%10) -> "через ${Math.abs(secondsDiff)/(60*60)} час"
+        secondsDiff<0 && Math.abs(secondsDiff) in 75*60..22*60*60 && range3.contains(Math.abs(secondsDiff)/(60*60)%10) -> "через ${Math.abs(secondsDiff)/(60*60)} часа"
+        secondsDiff<0 && Math.abs(secondsDiff) in 22*60*60..26*60*60 -> "день назад"
+        secondsDiff<0 && Math.abs(secondsDiff) in 26*60*60..360*24*60*60 && Math.abs(secondsDiff)<=-10*24*60*60 && Math.abs(secondsDiff)>=-20*24*60*60 -> "через ${Math.abs(secondsDiff)/(24*60*60)} дней"
+        secondsDiff<0 && Math.abs(secondsDiff) in 26*60*60..360*24*60*60 && range1.contains(Math.abs(secondsDiff)/(24*60*60)%10) -> "через ${Math.abs(secondsDiff)/(24*60*60)} дней"
+        secondsDiff<0 && Math.abs(secondsDiff) in 26*60*60..360*24*60*60 && range2.contains(Math.abs(secondsDiff)/(24*60*60)%10) -> "через ${Math.abs(secondsDiff)/(24*60*60)} день"
+        secondsDiff<0 && Math.abs(secondsDiff) in 26*60*60..360*24*60*60 && range3.contains(Math.abs(secondsDiff)/(24*60*60)%10) -> "через ${Math.abs(secondsDiff)/(24*60*60)} дня"
+        secondsDiff<0 && Math.abs(secondsDiff)>360*24*60*60 -> "более чем через год"
+        else -> "$secondsDiff"
+    }
+    return humanized
+}
+
+enum class TimeUnits{
+    SECOND, MINUTE, HOUR, DAY
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,99 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?,String?>{
+        val parts:List<String>? = fullName?.split(" ")
+        println("parts: $parts")
+        val firstName = if (parts?.getOrNull(0)=="") null else {parts?.getOrNull(0)}
+        val lastName = if (parts?.getOrNull(1)=="") null else {parts?.getOrNull(1)}
+
+        return firstName to lastName
+    }
+    fun transliteration(payload:String, divider:String=" "):String{
+        var translatedPayload =""
+        for (char in payload){
+            var shouldCapitalise = false
+            if (char.isUpperCase()){
+               shouldCapitalise = true
+            }
+
+            var translatedChar=when(char.toString().toLowerCase()){
+                " "->divider
+                "а" -> "a"
+
+                "б" ->"b"
+
+                "в"-> "v"
+
+                "г"-> "g"
+
+                "д"-> "d"
+
+                "е"->"e"
+
+                "ё"->"e"
+
+                "ж" ->"zh"
+
+                "з"->"z"
+
+                "и" -> "i"
+
+                "й"-> "i"
+
+                "к"->"k"
+
+                "л"-> "l"
+
+                "м"->"m"
+
+                "н"->"n"
+
+                "о"->"o"
+
+                "п"->"p"
+
+                "р"->"r"
+
+                "с"->"s"
+
+                "т"->"t"
+
+                "у"-> "u"
+
+                "ф"-> "f"
+
+                "х"-> "h"
+
+                "ц"-> "c"
+
+                "ч"->"ch"
+
+                "ш"->"sh"
+
+                "щ"-> "sh'"
+
+                "ъ"-> ""
+
+                "ы"-> "i"
+
+                "ь"-> ""
+
+                "э"-> "e"
+
+                "ю"-> "yu"
+
+                "я"-> "ya"
+                else ->char.toString()
+            }
+            translatedPayload += if (shouldCapitalise)translatedChar.capitalize() else translatedChar
+        }
+        return translatedPayload
+    }
+    fun toInitials(firstName:String?, lastName:String?):String?{
+        val firstLetter = if(firstName?.length==0) "" else firstName?.get(0)?.toUpperCase()?:""
+        val secondLetter = if(lastName?.length==0) "" else lastName?.get(0)?.toUpperCase()?:""
+        var initials = if("$firstLetter$secondLetter"=="") null else{"$firstLetter$secondLetter"}
+        return initials
+    }
+}
